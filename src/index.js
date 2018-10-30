bfc07d0560ee8e504ba2881ae1528f63f69328a9
Durga Prasad <durgaprasad@meratransport.com>
2018-10-30 09:46:18 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(
-    <div className="container">
-    <div className="row">
-    <p>Hello world</p>
-    </div>
-    </div>
-);
-   
+import {HashRouter, Route, Switch} from 'react-router-dom';
+// import Full from './containers/Full/'
+// import Full from '../../containers/Full'
 
+import kayzoonga from './views/kayzoonga/kayzoonga'
+import { Provider } from "react-redux";
+import  store  from './store';
+import { bindActionCreators } from 'redux';
+ReactDOM.render(
+
+    <Provider store={store}>
+      <HashRouter>
+          <Switch>
+            <Route exact path="/" name="kayzoonga" component={kayzoonga} />
+          </Switch>
+       
+        
+        }
+       
+      </HashRouter>
+    </Provider>
+    
+    , document.getElementById('root'));
---
+++ src/views/kayzoonga/kayzoonga.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+    Badge,
+    Row,
+    Col,
+    Progress,
+    Dropdown,
+    Button,
+    ButtonToolbar,
+    ButtonGroup,
+    ButtonDropdown,
+    Label,
+    Input,
+    Table,
+    Form,
+    FormGroup,
+    FormText,
+    Nav, NavItem, NavLink, TabContent, TabPane
+} from 'reactstrap';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+
+class kayzoonga extends Component {
+
+    render() {
+        
+
+    }
+
+}
+export default kayzoonga;
